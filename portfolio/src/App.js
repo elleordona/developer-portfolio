b0611b8bdd8f00bd4281a26dce46d7e202033d98
Elle Ordona <eto0507@hotmail.co.uk>
2023-04-07 20:55:24 +0100

    started landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Stack from 'react-bootstrap/Stack';
 
 import AppNavbar from './Components/Navbar';
 import LandingPage from './Components/LandingPage';
@@ -8,16 +9,17 @@ import LandingPage from './Components/LandingPage';
 function App() {
 	return (
 		<div className="App">
-			<header>
-				<AppNavbar />
-			</header>
-			<div className="main">
-				<Routes>
-					<Route path="/" element={<LandingPage />} />
-				</Routes>
-			</div>
-
-			<footer className="footer mx-10vw">Elle Ordoña 2023</footer>
+			<Stack gap={3}>
+				<div>
+					<AppNavbar />
+				</div>
+				<div className="main">
+					<Routes>
+						<Route path="/" element={<LandingPage />} />
+					</Routes>
+					<footer className="footer mx-10vw">Elle Ordoña 2023</footer>
+				</div>
+			</Stack>
 		</div>
 	);
 }
--- portfolio/src/Components/LandingPage.jsx
+++ portfolio/src/Components/LandingPage.jsx
@@ -2,9 +2,38 @@
 
 // imports
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import './styles/LandingPage.css';
+import picOfMe from '../images/Asuna_SAO.png';
 
 const LandingPage = () => {
-	return <div>landing page</div>;
+	return (
+		<div>
+			<Container fluid>
+				<Row>
+					<Col>
+						<Image src={picOfMe} alt="Elle Ordona" className="img" />
+					</Col>
+					<Col>
+						<h2>Hi, I'm Elle Ordoña. I am Full Stack Developer starting my journey in the tech industry.</h2>
+						<h4>If I'm not at home on my computer you will usually find me in the gym, playing basketball or with my family.</h4>
+						<span>Learn more about me </span>
+						<span>
+							<Button size="sm" variant="link" className="aboutMeBtn" href="/about">
+								<FontAwesomeIcon icon={faArrowRight} />
+							</Button>
+						</span>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default LandingPage;
