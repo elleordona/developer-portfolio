d7a454b2ee0558f71d4039c2835a5d53b7841d2b
Elle Ordona <eto0507@hotmail.co.uk>
2023-04-09 23:25:13 +0100

    made site viewable for mobile (will add different views for larger screens later)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Stack from 'react-bootstrap/Stack';
 
 import AppNavbar from './Components/Navbar';
 import LandingPage from './Components/LandingPage';
+import DarkModeButton from './Components/DarkModeButton';
 
 function App() {
 	return (
 		<div className="App">
-			<Stack gap={3}>
-				<div>
-					<AppNavbar />
-				</div>
-				<div className="main">
-					<Routes>
-						<Route path="/" element={<LandingPage />} />
-					</Routes>
-				</div>
-				<footer className="footer">Elle Ordoña 2023</footer>
-			</Stack>
+			<AppNavbar />
+
+			<div className="main">
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+				</Routes>
+			</div>
+			<footer className="footer">
+				Elle Ordoña 2023
+				<DarkModeButton />
+			</footer>
 		</div>
 	);
 }
--- portfolio/src/Components/LandingPage.jsx
+++ portfolio/src/Components/LandingPage.jsx
@@ -6,8 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import './styles/LandingPage.css';
 import picOfMe from '../images/Asuna_SAO.png';
@@ -15,22 +15,17 @@ import picOfMe from '../images/Asuna_SAO.png';
 const LandingPage = () => {
 	return (
 		<div>
-			<Container fluid>
-				<Row>
-					<Col>
-						<Image src={picOfMe} alt="Elle Ordona" className="img" rounded />
-					</Col>
-					<Col>
-						<h2>Hi, I'm Elle Ordoña. I am Full Stack Developer starting my journey in the tech industry.</h2>
-						<h4>If I'm not at home on my computer you will usually find me in the gym, playing basketball or with my family.</h4>
-						<span>Learn more about me </span>
-						<span>
-							<Button size="sm" variant="link" className="aboutMeBtn" href="/about">
-								<FontAwesomeIcon icon={faArrowRight} />
-							</Button>
-						</span>
-					</Col>
-				</Row>
+			<Container fluid="md">
+				<Image src={picOfMe} alt="Elle Ordona" className="img" rounded />
+
+				<h2>Hi, I'm Elle Ordoña. I am Full Stack Developer starting my journey in the tech industry.</h2>
+				<h5>If I'm not at home on my computer coding or playing games, you will usually find me in the gym, playing basketball, or with my family.</h5>
+				<span>Learn more about me </span>
+				<span>
+					<Button size="sm" variant="link" className="aboutMeBtn" href="/about">
+						<FontAwesomeIcon icon={faArrowRight} />
+					</Button>
+				</span>
 			</Container>
 		</div>
 	);
