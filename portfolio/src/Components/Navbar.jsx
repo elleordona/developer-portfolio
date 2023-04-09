0093eb1fa4da73b887558a796e50a7ce922d5284
Elle Ordona <eto0507@hotmail.co.uk>
2023-04-09 23:23:38 +0100

    collapsible and responsive navbar

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,69 +1,46 @@
 // Navbar Component
 
 // imports
-import React, { useState } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Container, Button, InputGroup } from 'react-bootstrap';
-import { ThemeContext, themes } from '../utils/themeContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Container } from 'react-bootstrap';
+
 import './styles/Navbar.css';
 
 const AppNavbar = () => {
-	const [darkMode, setDarkMode] = useState(true);
-
 	return (
-		<Navbar fixed="top">
-			<Nav className="m-auto">
+		<Navbar collapseOnSelect expand="lg" className="navbar">
+			<Container>
 				<NavbarBrand href="/" className="nav-brand">
-					Elle Ordoña
+					<b>Elle Ordoña</b>
 				</NavbarBrand>
-			</Nav>
 
-			<Nav>
-				<NavItem>
-					<NavLink className="link" href="/">
-						Home
-					</NavLink>
-				</NavItem>
-				<NavItem>
-					<NavLink className="link" href="/projects">
-						Projects
-					</NavLink>
-				</NavItem>
-				<NavItem>
-					<NavLink className="link" href="/about">
-						About Me
-					</NavLink>
-				</NavItem>
-				<NavItem>
-					<NavLink className="link" href="/contact">
-						Contact
-					</NavLink>
-				</NavItem>
-			</Nav>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-			<Nav className="m-auto">
-				<NavItem>
-					<Container>
-						<InputGroup>
-							<ThemeContext.Consumer>
-								{({ changeTheme }) => (
-									<Button
-										variant={darkMode ? 'outline-light' : 'outline-dark'}
-										onClick={() => {
-											setDarkMode(!darkMode);
-											changeTheme(darkMode ? themes.light : themes.dark);
-										}}
-										size="sm"
-									>
-										<FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
-									</Button>
-								)}
-							</ThemeContext.Consumer>
-						</InputGroup>
-					</Container>
-				</NavItem>
-			</Nav>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+						<NavItem>
+							<NavLink className="link" href="/">
+								Home
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className="link" href="/projects">
+								Projects
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className="link" href="/about">
+								About Me
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className="link" href="/contact">
+								Contact
+							</NavLink>
+						</NavItem>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
 		</Navbar>
 	);
 };
