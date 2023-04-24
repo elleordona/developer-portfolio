a4a99d819cc1d64db815921b116676a91b334555
Elle Ordoña <eto0507@hotmail.co.uk>
2023-04-24 13:39:15 +0100

    created suthServices

---
+++ portfolio/src/utils/auth.service.js
@@ -0,0 +1,57 @@
+//* functions for signup/in and log out
+
+// imports
+import axios from 'axios';
+
+const API_URI = `${process.env.REACT_APP_DB}/api/auth/`;
+
+const register = async (name, username, password, email) => {
+	try {
+		const response = await axios.post(`${API_URI}signup`, {
+			name,
+			username,
+			password,
+			email,
+		});
+
+		const data = response.data;
+
+		return data;
+	} catch (e) {
+		return { error: e.response.data.message };
+	}
+};
+
+const login = async (username, password) => {
+	try {
+		const response = await axios.post(`${API_URI}signin`, {
+			username,
+			password,
+		});
+		const data = await response.data;
+		if (data.accessToken) {
+			localStorage.setItem(`user`, JSON.stringify(response.data));
+		}
+
+		return data;
+	} catch (e) {
+		return { error: e.response.data.message };
+	}
+};
+
+const logout = () => {
+	localStorage.removeItem(`user`);
+};
+
+const getCurrentUser = () => {
+	return JSON.parse(localStorage.getItem(`user`));
+};
+
+const authService = {
+	register,
+	login,
+	logout,
+	getCurrentUser,
+};
+
+export default authService;
