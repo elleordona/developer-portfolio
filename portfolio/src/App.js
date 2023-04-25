e6203d2409540547fe00eb259fa4b5ef7984d50f
Elle Ordoña <eto0507@hotmail.co.uk>
2023-04-25 12:57:33 +0100

    created register component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import AppNavbar from './Components/Navbar';
 import LandingPage from './Components/LandingPage';
 import Login from './Components/Login';
+import Register from './Components/Register';
 import Footer from './Components/Footer';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 					<Route index element={<LandingPage />} />
 					<Route path="/home" element={<LandingPage />} />
 					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
 				</Routes>
 			</div>
 			<hr />
---
+++ portfolio/src/Components/Register.jsx
@@ -0,0 +1,97 @@
+//* Register Component
+
+// imports
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import authService from '../utils/auth.service';
+import validationService from '../utils/validation.serviceHelpers';
+
+const Register = () => {
+	// const form = useRef();
+	// const checkBtn = useRef();
+
+	const [username, setUsername] = useState(``);
+	const [email, setEmail] = useState(``);
+	const [password, setPassword] = useState(``);
+	const [successful, setSuccessful] = useState(false);
+	const [message, setMessage] = useState(``);
+
+	const onChangeUsername = (e) => {
+		const username = e.target.value;
+		setUsername(username);
+	};
+
+	const onChangePassword = (e) => {
+		const password = e.target.value;
+		setPassword(password);
+	};
+
+	const onChangeEmail = (e) => {
+		const email = e.target.value;
+		setEmail(email);
+	};
+
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (successful) navigate(`/`);
+	}, [successful, navigate]);
+
+	const handleRegister = async (e) => {
+		e.preventDefault();
+
+		setMessage(``);
+		setSuccessful(false);
+
+		// form.current.validateAll();
+
+		if (username && password && email) {
+			const register = await authService.register(username, email, password);
+			if (register.message) {
+				setMessage(register.message);
+				setSuccessful(true);
+			} else {
+				setMessage(register.error);
+				setSuccessful(false);
+			}
+		}
+	};
+
+	return (
+		<div className="card w-75 mx-auto text-center justify-content-center p-4">
+			<h3>Create a New Account</h3>
+			<form onSubmit={handleRegister}>
+				{!successful && (
+					<div>
+						<div className="form-group">
+							<label htmlFor="username">Username:</label>
+							<input type="text" name="username" id="username" className="form-control" value={username} onChange={onChangeUsername} validations={[validationService.required, validationService.validUsername]} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="email">Email:</label>
+							<input type="email" name="email" id="email" className="form-control" value={email} onChange={onChangeEmail} validations={[validationService.required, validationService.validEmail]} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="password">Password:</label>
+							<input type="password" name="password" id="password" className="form-control" value={password} onChange={onChangePassword} validations={[validationService.required, validationService.validPassword]} />
+						</div>
+						<div className="form-group">
+							<button type="submit" className="btn">
+								Sign Up
+							</button>
+						</div>
+					</div>
+				)}
+				{message && (
+					<div className="form-group">
+						<div className={successful ? `alert alert-success` : `alert alert-danger`} role="alert">
+							{message}
+						</div>
+					</div>
+				)}
+			</form>
+		</div>
+	);
+};
+
+export default Register;
